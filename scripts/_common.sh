dec993ec1ba4064be1905ca1d80c94af8ded37eb
eric_G <46165813+ericgaspar@users.noreply.github.com>
2026-07-10 20:00:43 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,6 @@
 #=================================================
 
 install_mailpit_binary() {
-	ynh_script_progression "Provisioning Mailpit binary source..."
 	mkdir -p "$install_dir"
 	ynh_safe_rm "$install_dir/mailpit"
 	ynh_setup_source --source_id="main" --dest_dir="$install_dir"
